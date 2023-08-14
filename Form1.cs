b36cadbfb2e160f718a06bfc2f00f76dd237016a
Érik Eiji <eriknibe1@gmail.com>
2023-08-14 14:59:37 -0300

    feat: add StartTheQuiz function

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,49 @@ namespace MathQuiz
         {
             InitializeComponent();
         }
+
+        Random randomizer = new Random();
+
+        int addend1, addend2;
+        int minuend, subtrahend;
+        int multiplicand, multiplier;
+        int dividend, divisor;
+
+        public void StartTheQuiz()
+        {   
+            // Fill in the addition problem
+            // Generate two random numbers
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            // Convert the generated numbers into strings to be displayed in the labels controls
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+             
+            // Makes sure the 'sum' NumericUpDown's value starts at zero
+            sum.Value = 0;
+
+            // Fill in the subtraction problem
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            // Fill in the multiplication problem
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            // Fill in the division problem
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+        }
     }
 }
